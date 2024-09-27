7c6ccd2d71693b8ec1d1c1c94fd634b9cc189462
Ico-code <nico.nikusti@gmail.com>
2024-09-27 19:31:56 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'myAppLab2',
+  webDir: 'dist'
+};
+
+export default config;
